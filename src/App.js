c9e75c603b2270dcc00982cfd9a99a3581490b15
Dylan Kroft <dylankroft@gmail.com>
2022-09-16 14:59:08 +0700

    hi

--- src/App.js
+++ src/App.js
@@ -12,9 +12,10 @@ function App() {
   return (
     <Container>
       <Header/>
-        <img className='image' src={rules}/>
-        <img className='image' src={map}/>
-
+      <Regroup>PLEASE REGROUP AT 4:15</Regroup>
+      <img className='image' src={rules}/>
+      <img className='image' src={map}/>
+    
       <Table/>
     </Container>
   );
@@ -24,5 +25,19 @@ export default App;
 
 
 const Container = styled.div`
+
+`
+
+const Regroup = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+padding: 40px;
+font-size: 3em;
+font-weight: 800;
+-webkit-box-shadow: rgba(0, 0, 0, 0.3) 0 1px 3px;
+  -moz-box-shadow: rgba(0,0,0,0.3) 0 1px 3px;
+  box-shadow: rgba(0, 0, 0, 0.3) 0 1px 3px;
+  background-color: rgb(250, 200, 0);
 
 `
